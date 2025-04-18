486a38f31abadae5a8d0995e4191648428f0176d
Vladislav Aleinik <valeinik@ispras.ru>
2025-04-18 16:18:11 +0300

    examples: add example 31_conditions_branches and slides

---
+++ examples/31_conditions_branches/src/benchmark.c
@@ -0,0 +1,105 @@
+#include <stdint.h>
+
+int      io_get_dec(void);
+unsigned io_get_hex(void);
+
+void io_print_dec(int v);
+void io_print_hex(unsigned v);
+
+void io_print_string(const char *s);
+
+void io_newline(void);
+
+// Задание: изучите бинарный код этой функции при помощи дизассемблера.
+void example_conditionals(void)
+{
+    static uint32_t u32_a, u32_b;
+    static  int32_t s32_c, s32_d;
+
+    if (u32_a > u32_b)
+    {
+        return;
+    }
+
+    if (u32_a != u32_b)
+    {
+        return;
+    }
+
+    if (u32_a <= u32_b && u32_a >= u32_b)
+    {
+        return;
+    }
+
+    if (s32_c == s32_d)
+    {
+        return;
+    }
+
+    if (s32_c > s32_d)
+    {
+        return;
+    }
+
+    if (s32_c >= s32_d || s32_c > s32_d)
+    {
+        return;
+    }
+}
+
+typedef enum
+{
+    OP_ADD,
+    OP_SUB,
+    OP_MUL,
+    OP_DIV,
+    OP_SIN,
+    OP_COS,
+    OP_EXP
+} OperationType;
+
+// Задание: изучите бинарный код этой функции при помощи дизассемблера.
+void example_ifelse_chain(void)
+{
+    static OperationType op;
+    static int result;
+
+    if      (op == OP_ADD) { result = 0; }
+    else if (op == OP_SUB) { result = 1; }
+    else if (op == OP_MUL) { result = 2; }
+    else if (op == OP_DIV) { result = 3; }
+    else if (op == OP_SIN) { result = 4; }
+    else if (op == OP_COS) { result = 5; }
+    else if (op == OP_EXP) { result = 6; }
+}
+
+// Задание: изучите бинарный код этой функции при помощи дизассемблера.
+void example_switch(void)
+{
+    static OperationType type;
+    static int result;
+
+    switch (type)
+    {
+    case OP_ADD:
+        result = 0;
+    case OP_SUB:
+        result = 1;
+        break;
+    case OP_MUL:
+        result = 2;
+        break;
+    case OP_DIV:
+        result = 3;
+        break;
+    case OP_SIN:
+        result = 4;
+        break;
+    case OP_COS:
+        result = 5;
+        break;
+    case OP_EXP:
+        result = 6;
+        break;
+    }
+}
---
+++ examples/31_conditions_branches/src/main.asm
@@ -0,0 +1,91 @@
+bits 32
+
+extern io_print_udec, io_get_udec, io_print_string, io_newline
+
+section .text
+global main
+main:
+    ;====================;
+    ; Операции сравнения ;
+    ;====================;
+
+    ; eax = *u32_a
+    ; ebx = *u32_b
+    mov eax, dword [u32_a]
+    mov ebx, dword [u32_b]
+
+    ; Инструкция сравнения.
+    cmp eax, ebx
+    ; CFLAGS: SF=? ZF=? OF=? CF=?
+
+    ; Инструкция вычитания.
+    sub eax, ebx
+    ; CFLAGS: SF=? ZF=? OF=? CF=?
+
+    ;==============================;
+    ; Рассчёт N-го числа Фибоначчи ;
+    ;==============================;
+
+    ; Выводим приветственную строку.
+    mov     eax, inputstr
+    call    io_print_string
+
+    ; Считываем номер числа Фибоначчи для рассчёта.
+    call    io_get_udec
+    ; ESI - номер итогового числа Фибоначчи
+    mov     esi, eax
+
+    ; Выводим нулевое число Фибоначии
+    cmp     esi, 0
+    jne     init_fibs_computation
+
+    mov     eax, 0
+    call    io_print_udec
+    call    io_newline
+    jmp     exit
+
+init_fibs_computation:
+    ; ECX - номер текущего числа Фибоначчи
+    mov     ecx, 0
+    ; EDI - предыдущее число Фибоначчи
+    mov     edi, 0
+    ; EBX - текущее число Фибоначчи
+    mov     ebx, 1
+compute_next_fibs:
+    ; EDI := EBX
+    ; EBX := EDI + EBX
+    add     edi, ebx
+    xchg    edi, ebx
+
+    ; Производим проверку на переполнение.
+    jc      detect_overflow
+
+    inc     ecx
+    cmp     ecx, esi
+    jl     compute_next_fibs
+
+    ; Выводим результирующее число Фибоначчи.
+    mov     eax, outputstr
+    call    io_print_string
+
+    mov     eax, edi
+    call    io_print_udec
+    call    io_newline
+    jmp     exit
+
+detect_overflow:
+    mov     eax, overflowstr
+    call    io_print_string
+
+exit:
+    xor eax, eax
+    ret
+
+section .rodata
+inputstr:       db `Enter a number: `, 0
+outputstr:      db `Fibinacci number is: `, 0
+overflowstr:    db `Detected overflow!`, 0xA, 0
+
+section .data
+u32_a   dd      10
+u32_b   dd      15
