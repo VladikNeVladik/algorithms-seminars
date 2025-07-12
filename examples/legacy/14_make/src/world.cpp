c697d0b36f6b96e48187229d7ef905476dcf67cb
Vladislav Aleinik <valeinik@ispras.ru>
2025-07-12 23:12:59 +0300

    examples: remove legacy examples

--- examples/legacy/14_make/src/world.cpp
+++
@@ -1,6 +0,0 @@
-#include <helloworld.hpp>
-
-int main()
-{
-	print_helloworld();
-}
--- examples/legacy/15_from_c_to_cpp/src/utils.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2024 Vladislav Aleinik
-
-#include <utils.hpp>
-
-// NOTE: allocate only one buffer for all object files.
-char explanation_str[EXCEPTION_STR_SIZE];
